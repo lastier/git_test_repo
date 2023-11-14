4687ac88aff93049ee7a4dd1282684cfa36b8191
HYEONSU <HYEONSU@host.docker.internal>
2023-11-14 20:01:01 +0900

    java flow control

---
+++ java_flow_control/java_flow_control/BooleanApp.java
@@ -0,0 +1,24 @@
+package java_flow_control;
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("One"); //문자 타입
+		System.out.println(1); //숫자(정수)타입
+		
+		System.out.println(true);
+		System.out.println(false);
+		//reserved word
+		
+		
+		String foo = "Hello World";
+		System.out.println(foo.contains("World")); 
+		System.out.println(foo.contains("egoing"));
+		
+		
+
+	}
+
+}
---
+++ java_flow_control/java_flow_control/ComparisonOperatorApp.java
@@ -0,0 +1,15 @@
+package java_flow_control;
+
+public class ComparisonOperatorApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(1 > 1); //false
+		System.out.println(1 == 1);// true
+		System.out.println(1 < 1);
+		System.out.println(1 >= 1);
+
+	}
+
+}
