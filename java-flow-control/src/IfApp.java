a7419ddad494fc3e2c9783ae1fdc289d1fca255d
HYEONSU <kwenhs0525@naver.com>
2023-11-17 00:50:26 +0900

    "boolean & while & for"

---
+++ java-flow-control/src/IfApp.java
@@ -0,0 +1,19 @@
+
+public class IfApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+ */
+		System.out.println("a");
+		
+		if(true) {
+			System.out.println(1); 
+		} else if(false) {
+			System.out.println(2);
+		} else {
+			System.out.println(3);
+		}
+		System.out.println("b");
+	}		
+}
